87c36118544a5921a97b7b8852ee36e18c88cd4c
Marc Lepage <mlepage@antimeta.com>
2015-12-12 18:45:21 -0500

    Add prototype ship control

--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -9,8 +9,12 @@ public class ShipControl : MonoBehaviour {
 	}
 	
 	void Update() {
-		if (Input.GetKeyDown(KeyCode.Q)) {
-			GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
+		if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.W)) {
+			GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speed);
+		} else if (Input.GetKey(KeyCode.Q)) {
+			GetComponent<Rigidbody>().AddTorque(Vector3.up * -speed);
+		} else if (Input.GetKey(KeyCode.W)) {
+			GetComponent<Rigidbody>().AddTorque(Vector3.up * speed);
 		}
 	}
 }
