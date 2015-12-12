29e044b8ffae498428ff2f52d88d9bef8751e38b
Marc Lepage <mlepage@antimeta.com>
2015-12-12 18:24:57 -0500

    Change coordinate system to flip Y and Z

---
+++ Assets/Scripts/ShipControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipControl : MonoBehaviour {
+
+	public float speed;
+
+	void Start() {
+	}
+	
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
+		}
+	}
+}
