5b69c519753157818f2fb2b783d8e98a0071116c
Marc Lepage <mlepage@antimeta.com>
2015-12-13 14:55:45 -0500

    Reset asteroid after scoring goal

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -5,11 +5,19 @@ using System.Collections;
 public class Goal : MonoBehaviour {
 
 	public Text scoreText;
+	public GameObject asteroid;
 
 	void OnTriggerEnter(Collider other) {
+		// Increase score
 		int score = 0;
 		int.TryParse(scoreText.text, out score);
 		++score;
 		scoreText.text = score.ToString();
+
+		// Reset asteroid
+		Rigidbody rigidBody = asteroid.GetComponent<Rigidbody>();
+		rigidBody.velocity = Vector3.zero;
+		rigidBody.angularVelocity = Vector3.zero;
+		asteroid.transform.position = Vector3.zero;
 	}
 }
