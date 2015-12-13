14312d07498dc4c844fd52dbea11c7fedc2cd4f5
Marc Lepage <mlepage@antimeta.com>
2015-12-13 15:28:38 -0500

    Add title card that fades out

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fader : MonoBehaviour {
+
+	public float delay = 3f;
+	public float time = 2f;
+
+	float elapsed = 0f;
+
+	Text text;
+
+	void Start() {
+		text = GetComponent<Text>();
+	}
+
+	void Update() {
+		elapsed += Time.deltaTime;
+		if (elapsed < delay + time) {
+			if (delay <= elapsed) {
+				// Fade out
+				text.color = Color.Lerp(Color.white, Color.clear, (elapsed - delay) / time);
+			}
+		} else {
+			// Done fading
+			Destroy(gameObject);
+		}
+	}
+}
