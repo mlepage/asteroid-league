4f19caef4caa66047b5b2a57289c343794c51604
Marc Lepage <mlepage@antimeta.com>
2015-12-12 19:20:24 -0500

    Add thrust to ship

--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -3,18 +3,28 @@ using System.Collections;
 
 public class ShipControl : MonoBehaviour {
 
+	public ThrustControl tcLeft;
+	public ThrustControl tcRight;
+
 	public float speed;
 
-	void Start() {
-	}
-	
 	void Update() {
+		float thrustLeft = 0f;
+		float thrustRight = 0f;
+
 		if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.W)) {
 			GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speed);
+			thrustLeft = 1f;
+			thrustRight = 1f;
 		} else if (Input.GetKey(KeyCode.Q)) {
 			GetComponent<Rigidbody>().AddTorque(Vector3.up * -speed);
+			thrustRight = 1f;
 		} else if (Input.GetKey(KeyCode.W)) {
 			GetComponent<Rigidbody>().AddTorque(Vector3.up * speed);
+			thrustLeft = 1f;
 		}
+
+		tcLeft.UpdateThrust(thrustLeft);
+		tcRight.UpdateThrust(thrustRight);
 	}
 }
---
+++ Assets/Scripts/ThrustControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrustControl : MonoBehaviour {
+
+	public void UpdateThrust(float thrust) {
+		transform.localScale = new Vector3(thrust, thrust, thrust);
+		if (thrust != 0f) {
+			transform.Rotate(new Vector3 (0f, 0f, Random.Range(0f, 360f)));
+		}
+	}
+}
