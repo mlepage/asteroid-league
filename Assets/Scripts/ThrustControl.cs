9cab49b7febaf2eee2d7b8db96dabbfd8af7ddf5
Marc Lepage <mlepage@antimeta.com>
2015-12-12 22:29:48 -0500

    Improve thrust visual effect

--- Assets/Scripts/ThrustControl.cs
+++ Assets/Scripts/ThrustControl.cs
@@ -7,6 +7,10 @@ public class ThrustControl : MonoBehaviour {
 		transform.localScale = new Vector3(thrust, thrust, thrust);
 		if (thrust != 0f) {
 			transform.Rotate(new Vector3 (0f, 0f, Random.Range(0f, 360f)));
+			Material material = GetComponentInChildren<Renderer>().material;
+			Color color = material.color;
+			color.a = Random.value;
+			material.color = color;
 		}
 	}
 }
