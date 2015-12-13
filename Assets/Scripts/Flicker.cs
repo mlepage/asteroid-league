8866d09b65479b372d7514fc9d4b5887d6c95814
Marc Lepage <mlepage@antimeta.com>
2015-12-13 01:56:25 -0500

    Make goals flicker

---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+
+	public float min = 0f;
+	public float max = 1f;
+
+	Material material;
+
+	void Start() {
+		material = GetComponentInChildren<Renderer>().material;
+	}
+
+	void Update() {
+		Color color = material.color;
+		color.a = Random.Range(min, max);
+		material.color = color;
+	}
+}
--- Assets/Scripts/ThrustControl.cs
+++ Assets/Scripts/ThrustControl.cs
@@ -7,10 +7,6 @@ public class ThrustControl : MonoBehaviour {
 		transform.localScale = new Vector3(thrust, thrust, thrust);
 		if (thrust != 0f) {
 			transform.Rotate(new Vector3 (0f, 0f, Random.Range(0f, 360f)));
-			Material material = GetComponentInChildren<Renderer>().material;
-			Color color = material.color;
-			color.a = Random.value;
-			material.color = color;
 		}
 	}
 }
