48dc262c25d8b3c0e4bf418d23f4fb16cd42b996
Marc Lepage <mlepage@antimeta.com>
2015-12-13 14:28:37 -0500

    Add score UI and goals

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	public Text scoreText;
+
+	void OnTriggerEnter(Collider other) {
+		int score = 0;
+		int.TryParse(scoreText.text, out score);
+		++score;
+		scoreText.text = score.ToString();
+	}
+}
