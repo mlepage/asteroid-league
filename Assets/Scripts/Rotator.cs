1fd1baeb3ad9225f3484ff63863738aa3c3871f1
Marc Lepage <mlepage@antimeta.com>
2015-12-13 01:35:55 -0500

    Make light rotate subtly

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float xSpeed;
+	public float ySpeed;
+	public float zSpeed;
+
+	void Update() {
+		transform.Rotate(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
+	}
+}
