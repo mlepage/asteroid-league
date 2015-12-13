ce1bd829321fa8defc70a1849fa6cc8c201f9921
Marc Lepage <mlepage@antimeta.com>
2015-12-13 17:21:32 -0500

    Add ball bounce sound

---
+++ Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnCollision : MonoBehaviour {
+
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+	}
+	
+	public void OnCollisionEnter(Collision collision) {
+		audio.Play();
+	}
+}
